b995b8b7d8c8c846eaa8dbb3003c4c1ac7dd3a52
dxol03 Lee <dlxogml024@naver.com>
2023-12-30 11:44:19 +0000

    new

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
+  String name;
+  System.out.println("your name");
+  name = sc.nextLine();
+  System.out.println("your name is " + name);
+  }
+
+}
