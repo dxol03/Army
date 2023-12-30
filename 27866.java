71e1a52014b0939b4a0ce26b181c59e50b3a38f8
dxol03 Lee <dlxogml024@naver.com>
2023-12-30 12:46:01 +0000

    Bronze

--- 27866.java
+++ 27866.java
@@ -3,9 +3,14 @@ import java.util.Scanner;
 public class 27866 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    
     String s = sc.nextLine();
     int i = sc.nextInt();
+    
+    // s.charAt(6) 문자열(String)의 6번째 자리수를 char 형식으로 받아옴
     char ch = s.charAt(i-1);
+
+    
     System.out.println(ch);
   }
 }
