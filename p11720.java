127d1af1b99f21482a2eb50124211d49a92e2e55
dxol03 Lee <dlxogml024@naver.com>
2023-12-30 13:16:47 +0000

    Bronzze

---
+++ p11720.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class p11720 {
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String n = sc.next();
+
+        int count = 0;
+
+        char[] ch = n.toCharArray();
+
+        for (int i = 0; i < ch.length; i++) {
+            int a = Character.getNumericValue(ch[i]);
+            count += a;
+        }
+        System.out.println(count);
+        
+    }
+}
--- p27866.java
+++ p27866.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class 27866 {
+public class p27866 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     
