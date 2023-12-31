1eeca6c3b953e150845fdd9a6348ad371a79c556
dxol03 Lee <dlxogml024@naver.com>
2023-12-31 08:11:38 +0000

    Unsolved

---
+++ P10209.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class P10209 { 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine();
+
+        char[] arr = {'a','b','c','d','e',''}
+
+        for (int i = 0; i < 26; i++) {
+            arr[i] = -1;
+        }
+        
+        for (int j = 0; j < S.length(); j++) {
+            for (int u = 0; u < 26; u++) {
+                
+            }
+        }
+
+    }
+    
+}
