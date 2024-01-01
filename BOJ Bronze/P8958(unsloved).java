46ed79a01945301fcb6a9e7b5b0116dd2a34566e
dxol03 Lee <dlxogml024@naver.com>
2024-01-01 11:43:44 +0000

    Bronze(Unsolved)

---
+++ BOJ Bronze/P8958(unsloved).java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class P8958 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+
+        
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < str.length(); j++) {
+                // 연속되면 for문으로 ++여러번 돌리기 
+                
+            }
+            
+        }
+     }
+}
