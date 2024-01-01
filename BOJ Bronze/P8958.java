a8f6d8491273f6240ab12fb35a8d4f912a52e2ac
dxol03 Lee <dlxogml024@naver.com>
2024-01-01 13:29:04 +0000

    Bronze

--- BOJ Bronze/P8958.java
+++ BOJ Bronze/P8958.java
@@ -8,17 +8,29 @@ public class P8958 {
         
         int n = Integer.parseInt(br.readLine());
 
-        
+        int count = 0;
+        int sum = 0;
+
         for (int i = 0; i < n; i++) {
             String str = br.readLine();
-            int ct = 0;
-            for (int j = 0; j < str.length(); j++) {
-                    if (str.charAt(i) == 'O') {
-
-                    }
-                
-            }
+            char[] ch = str.toCharArray();
             
-        }
+
+            for (int j = 0; j < ch.length; j++)  {
+                
+                if (ch[j] == 'O') {
+                    count += 1;
+                    sum += (count);
+
+                } else {
+                     count = 0;
+                }
+            }
+            System.out.println(sum);
+            sum = 0;
+            count = 0;
+        } 
+        
+        
      }
 }
