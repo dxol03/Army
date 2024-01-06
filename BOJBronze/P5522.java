264a1b8be4ba78b84cc978f9bca417479b10fae0
dxol03 Lee <dlxogml024@naver.com>
2024-01-06 14:55:49 +0000

    Bronze

---
+++ BOJBronze/P5522.java
@@ -0,0 +1,15 @@
+package BOJBronze;
+import java.util.Scanner;
+
+public class P5522 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int a1 = sc.nextInt();
+        int a2 = sc.nextInt();
+        int a3 = sc.nextInt();
+        int a4 = sc.nextInt();
+
+        System.out.println(a+a1+a2+a3+a4);
+    }
+}
