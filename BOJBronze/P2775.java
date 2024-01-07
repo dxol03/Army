66d3c45e14e740ac304638d3531e9aa008b5a6ae
dxol03 Lee <dlxogml024@naver.com>
2024-01-07 13:44:27 +0000

    Bronze

--- BOJBronze/P2775.java
+++ BOJBronze/P2775.java
@@ -4,19 +4,28 @@ public class P2775 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
+        int[][] dp = new int[15][15];
+        
+        
+        for (int h = 0; h < 15; h++) {
+        dp[0][h] = h;
+        }
 
-        int f0 = 0;
-
-        int back = 0;
-
-        for (int i = 0; i < (T*2); i++) {
+        
+        for (int i = 0; i < T; i++) {
             int k = Integer.parseInt(br.readLine());
             int n = Integer.parseInt(br.readLine());
             
-            
-
-
-
+            for (int u =1; u <= k; u++) {
+                for (int j = 1; j <= n; j++) {
+                    int sum =0;
+                    for(int l=1; l<=j; l++){
+                        sum += dp[u-1][l];
+                    }
+                    dp[u][j] = sum;
+                }
+            }
+                System.out.println(dp[k][n]);
             
         }
 
