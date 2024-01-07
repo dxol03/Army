207ef708cd7868335eb486d9fdb2353f41c10f99
dxol03 Lee <dlxogml024@naver.com>
2024-01-07 11:45:23 +0000

    Bronze

---
+++ BOJBronze/P2775.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class P2775 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        int f0 = 0;
+
+        int back = 0;
+
+        for (int i = 0; i < (T*2); i++) {
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+            
+            
+
+
+
+            
+        }
+
+    }
+}
