eda56df655ff6a90ec76a49ef5c5dcf9d639736f
dxol03 Lee <dlxogml024@naver.com>
2024-01-20 10:56:05 +0000

    SILVER

---
+++ BOJSILVER/P10828.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class P10828 {
+    public static void main(String[] args) throws IOException {
+        Stack<Integer> stack = new Stack<Integer>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            String command = st.nextToken();
+            if (command.equals("push")) {
+                stack.push(Integer.parseInt(st.nextToken()));
+            } else if (command.equals("pop")) {
+                if (stack.isEmpty()) {
+                    System.out.println("-1");
+                } else {
+                    System.out.println(stack.pop());
+                }
+            } else if (command.equals("size")) {
+                System.out.println(stack.size());
+            } else if (command.equals("empty")) {
+                if (stack.isEmpty()) {
+                    System.out.println("1");
+                } else {
+                    System.out.println("0");
+                }
+            } else if (command.equals("top")) {
+                if (stack.isEmpty()) {
+                    System.out.println("-1");
+                } else {
+                    System.out.println(stack.peek());
+                }
+            }
+            
+        }
+
+
+    }
+}
